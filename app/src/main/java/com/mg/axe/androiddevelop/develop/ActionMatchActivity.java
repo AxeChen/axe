7427278774cf50e9935c9769cad7e864cb728112
AxeChen <1137366723@qq.com>
2017-01-02 01:44:00 +0800

    update

--- app/src/main/java/com/mg/axe/androiddevelop/develop/ActionMatchActivity.java
+++ app/src/main/java/com/mg/axe/androiddevelop/develop/ActionMatchActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.mg.axe.androiddevelop.R;
@@ -23,5 +24,7 @@ public class ActionMatchActivity extends AppCompatActivity {
     public void matchA(View view){
         Intent intent = new Intent(Intent.ACTION_MEDIA_SHARED);
         startActivity(intent);
+
+        Log.i("yes","yse");
     }
 }
